a361b97d1f8c3cab1547108c2f26c90914d252bf
giovannipozzobon <giovanni.pozzobon@gmail.com>
2024-03-03 14:09:59 +0100

    added the rom and correct the u_int_ errors (changed in uint_). change the intellisensemode in macos-cloang-arm64

--- src/start_debugout.sh
+++ src/start_debugout.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-minicom --ansi --8bit --color=on --attrib=on --capturefile=c64.log -D /dev/ttyACM1
+minicom --ansi --8bit --color=on --attrib=on --capturefile=c64.log -D /dev/tty.usbmodem102
