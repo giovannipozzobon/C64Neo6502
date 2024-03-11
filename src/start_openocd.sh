aefbd364e0cde11b463cce535139f8b3fd9bfdc5
giovannipozzobon <giovanni.pozzobon@gmail.com>
2024-03-11 08:18:43 +0100

    Mac Osx compatibility for debug
    Mac Osx compatibility for debug.
    Modified start_openocd.sh and lanch.json

--- src/start_openocd.sh
+++ src/start_openocd.sh
@@ -1,1 +1,2 @@
-openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000"
+#openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000"
+openocd -f interface/cmsis-dap.cfg   -f target/rp2040.cfg -s tcl -c "adapter speed 5000" 
